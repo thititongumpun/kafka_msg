71a02402534f5657196aae5963476800dece199d
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-12-16 16:36:53 +0700

    change shellscript cli

--- confluent-fundamentals/labs/confluent-platform/start.sh
+++ confluent-fundamentals/labs/confluent-platform/start.sh
@@ -3,10 +3,10 @@
 docker-compose up -d
 
 echo "Waiting for Kafka to launch on 9092..."
-while ! nc -z kafka 9092; do   
-  sleep 1.0
-  echo "Kafka not yet ready..."
-done 
+# while ! nc -z kafka 9092; do   
+#   sleep 1.0
+#   echo "Kafka not yet ready..."
+# done 
 echo "Kafka is now ready!"
 
 kafka-topics --bootstrap-server kafka:9092 \
--- confluent-fundamentals/labs/fundamentals/start.sh
+++ confluent-fundamentals/labs/fundamentals/start.sh
@@ -3,8 +3,8 @@
 docker-compose up -d
 
 echo "Waiting for Kafka to launch on 9092..."
-while ! nc -z kafka 9092; do   
-  sleep 1.0
-  echo "Kafka not yet ready..."
-done 
+# while ! nc -z kafka 9092; do   
+#   sleep 1.0
+#   echo "Kafka not yet ready..."
+# done 
 echo "Kafka is now ready!"

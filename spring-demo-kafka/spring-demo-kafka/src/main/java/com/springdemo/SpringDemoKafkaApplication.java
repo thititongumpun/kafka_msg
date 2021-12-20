823cbf62481c352b349687f51c0f323e81bbb823
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-12-20 17:40:59 +0700

    add spring test kafka

---
+++ spring-demo-kafka/spring-demo-kafka/src/main/java/com/springdemo/SpringDemoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoKafkaApplication.class, args);
+	}
+
+}
---
+++ spring-demo-kafka/spring-demo-kafka/src/test/java/com/springdemo/springdemokafka/SpringDemoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.springdemokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
